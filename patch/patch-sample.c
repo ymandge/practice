b3100868d0b11f174ce28889b76ee1dfd28fe02a
ymandge <mandgeyogesh77@gmail.com>
2023-03-28 10:02:11 -0700

    Create patch demo

---
+++ patch/patch-sample.c
@@ -0,0 +1,9 @@
+--- sample.c.orig	2022-07-20 10:23:39.404803482 -0700
++++ sample.c	2022-07-20 10:24:32.933250210 -0700
+@@ -3,5 +3,6 @@
+ int main()
+ {
+ 	printf("this is original sample file\n");
++	printf("this is the patch change\n");
+ 	return 0;
+ }
